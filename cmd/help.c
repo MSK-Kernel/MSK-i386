1d628daffca4a56bd0c1f3c7492a7d3371d0379b
MSK <rhysg1414@gmail.com>
2026-06-29 13:49:42 -0700

    Update help.c

--- cmd/help.c
+++ cmd/help.c
@@ -1,26 +1,5 @@
-// cmd/help.c - Shows all available commands
+extern void list_commands(void);
 
-extern void print(const char* s);
-extern int cmd_count;
-
-// These are from kernel.c
-typedef struct {
-    char name[32];
-    void (*func)(char* args);
-} Command;
-
-extern Command cmd_table[];
-
-void help_cmd(char* args) {
-    print("\nAvailable commands:");
-    if (cmd_count == 0) {
-        return;
-    }
-    
-    for (int i = 0; i < cmd_count; i++) {
-        print("\n");
-        print(cmd_table[i].name);
-    }
-    
-    print("\n");
+void help(char* args) {
+    list_commands();
 }
